c5ff14269036589ff1cb325c4eba03bfcb2f5f07
Alessandro <alessandrovaiz@gmail.com>
2022-02-10 00:21:10 -0300

    feat: new routes

--- src/routes/_index.ts
+++ src/routes/_index.ts
@@ -2,10 +2,14 @@ import { Router } from 'express';
 import AuthController from '../controllers/authController';
 import authRoutes from './authRoutes';
 import userRoutes from './userRoutes';
+import batchRoutes from './batchRoutes';
+import eventRoutes from './eventRoutes';
 
 const router = Router();
 
 router.use(authRoutes);
 router.use(userRoutes);
+router.use(batchRoutes);
+router.use(eventRoutes);
 
 export default router;
