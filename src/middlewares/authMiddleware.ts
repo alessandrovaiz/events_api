f96e76453a0fcc030aca45ef9c028c864263ca61
Alessandro <alessandrovaiz@gmail.com>
2022-02-13 19:03:52 -0300

    fix: User id on authorization token

--- src/middlewares/authMiddleware.ts
+++ src/middlewares/authMiddleware.ts
@@ -2,13 +2,12 @@ import { Request, Response, NextFunction } from 'express'
 import jwt from 'jsonwebtoken'
 
 interface TokenPayload {
-    id: string
+    _id: string
     iat: number
     exp: number
 }
 export default function authMiddleware (req: Request, res: Response, next: NextFunction) {
   const { authorization } = req.headers
-
   if (!authorization) {
     return res.status(401).json({
       error: true,
@@ -20,8 +19,8 @@ export default function authMiddleware (req: Request, res: Response, next: NextF
 
   try {
     const data = jwt.verify(token, process.env.JWT_SECRET)
-    const { id } = data as TokenPayload
-    req.userId = id
+    const { _id } = data as TokenPayload
+    req.userId = _id
     return next()
   } catch (err: unknown) {
     return res.status(401).json({
